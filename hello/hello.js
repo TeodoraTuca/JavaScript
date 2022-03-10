7c86c095661c203681d56b4112a7cad0acbd178c
TeodoraTuca <95707880+TeodoraTuca@users.noreply.github.com>
2022-03-10 15:21:52 +0200

    hello

---
+++ hello/hello.js
@@ -0,0 +1,16 @@
+// ruleaza tellMe dupa 3000 ms(3s):
+setTimeout(tellMe, 3000)
+
+// creaza o noua instructiune (functie) numita tellMe
+function tellMe() {
+  alert("Hello world!")
+  alert("Welcome to JavaScript")
+}
+
+/*
+    comentariu pe 
+    mai multe linii
+*/
+
+// comentariu pentru o singura linie
+
